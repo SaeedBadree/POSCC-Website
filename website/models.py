04f379f792c03a62dcf6f01586dbe82e79b71e20
SaeedBadree <saeed.badree@my.uwi.edu>
2024-08-27 18:02:14 -0400

    Login and signup

---
+++ website/models.py
@@ -0,0 +1,26 @@
+from . import db
+from flask_login import UserMixin
+from itsdangerous import URLSafeTimedSerializer as Serializer  # Updated import
+from flask import current_app
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token, max_age=1800)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"<User {self.email}>"
