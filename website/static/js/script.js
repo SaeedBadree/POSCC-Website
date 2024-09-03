aaf4fe88574b3c8c457439b2acf853bf7460e68f
SaeedBadree <saeed.badree@my.uwi.edu>
2024-09-03 14:09:41 -0400

    further updates

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -1,8 +1,22 @@
-function myFunction() {
-    var x = document.getElementById("top-menu-bar");
-    if (x.className === "menu-bar") {
-      x.className += " responsive";
-    } else {
-      x.className = "menu-bar";
-    }
+
+document.addEventListener('scroll', function() {
+  const aboutSection = document.querySelector('.about-section');
+  const linkItems = document.querySelectorAll('.link-item');
+
+  const triggerPoint = window.innerHeight * 0.7; // Adjust as necessary
+
+  // Check About Section
+  if (aboutSection.getBoundingClientRect().top < triggerPoint) {
+      aboutSection.classList.add('scrolled');
   }
+
+  // Check Link Items
+  linkItems.forEach(item => {
+      if (item.getBoundingClientRect().top < triggerPoint) {
+          item.classList.add('scrolled');
+      }
+  });
+});
+
+
+
